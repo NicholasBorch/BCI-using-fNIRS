66e164092848f05267d9f0cdf9c153d938e8b889
Viet Nguyen <vietnhprivat@gmail.com>
2025-06-09 13:02:52 +0200

    Removed old deprecated code

--- finger_tapping/feature_preparation.py
+++ finger_tapping/feature_preparation.py
@@ -1,13 +1,6 @@
 import numpy as np
 from mne import Epochs
 
-# def split_activities(subject: Epochs) -> tuple[Epochs, Epochs, Epochs]:
-#     """ Takes the subject activities and splits it into categories"""
-#     control = subject['Control']
-#     left = subject['Tapping/Left']
-#     right = subject['Tapping/Right']
-#     return control, left, right
-
 def split_activities(subject: Epochs) -> tuple[Epochs, Epochs, Epochs]:
     """ Takes the subject activities and splits it into categories"""
     string_label = list(set(subject.annotations.description))
@@ -22,9 +15,6 @@ def get_labels_numeric(subject: Epochs) -> np.ndarray:
     """Gets numeric labels from activities for 1 subject"""
     return subject.events[:, -1]
 
-# def get_minimum_bound(control: Epochs, left: Epochs, right: Epochs) -> int:
-#     """Find the activity with min amount of epochs """
-#     return np.min([x.get_data().shape[0] for x in [control, left, right]])
 
 def get_minimum_bound(epoch_list:list) -> int:
     """Find the activity with min amount of epochs """
@@ -46,25 +36,6 @@ def create_labels(labels: list[str], lenght: int) -> np.ndarray:
     for label in labels:
         init_list.append(np.full(lenght, label))
     return np.concatenate(init_list)
-
-# def extract_X_y(subject: Epochs) -> tuple[np.ndarray, np.ndarray]:
-#     """Combines all functions in one function and creates X and y ~ (features and labels)"""
-    
-#     control, left, right = split_activities(subject)
-#     min_bound = get_minimum_bound(control, left, right)
-    
-#     control_reshaped = reshape_activity(control, min_bound)
-#     left_reshaped = reshape_activity(left, min_bound)
-#     right_reshaped = reshape_activity(right, min_bound)
-    
-#     # Define X input
-#     X = np.concatenate([control_reshaped, left_reshaped, right_reshaped], axis=0)
-
-#     # Create labels and define y
-#     lenght = control_reshaped.shape[0]
-#     labels = ['control', 'left','rigth']
-#     y = create_labels(labels, lenght)
-#     return X, y
 
 def extract_X_y(subject: Epochs) -> tuple[np.ndarray, np.ndarray]:
     """Combines all functions in one function and creates X and y ~ (features and labels)"""
